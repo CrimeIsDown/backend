2c7bfe0f7c7f8e1ad3fbcb3754d0b2bb2fe1e5b9
Eric Tendian <erictendian@gmail.com>
2019-01-23 01:50:18 -0600

    Refactoring to handle both .aac and .ogg recording files

--- app/Http/Controllers/AudioArchiveController.php
+++ app/Http/Controllers/AudioArchiveController.php
@@ -63,32 +63,38 @@ class AudioArchiveController extends Controller
 
         try {
             $this->validate($request, [
-                'feed'     => 'required|in:zone1,zone2,zone3,zone4,zone5,zone6,zone7,zone8,zone9,zone10,zone11,zone12,zone13,citywide1,citywide2,citywide6',
+                'feed'     => 'required|in:zone1,zone2,zone3,zone4,zone5,zone6,zone7,zone8,zone9,zone10,zone11,zone12,zone13,citywide1,citywide2,citywide4,citywide5,citywide6',
                 'datetime' => 'required|date'
             ]);
         } catch (ValidationException $e) {
             return response('Make sure you have selected a valid feed, and that your datetime is in the format '.Carbon::now()->minute(0)->second(0)->timezone('America/Chicago')->toDateTimeString(), 400);
         }
 
-        $file = $request->get('feed').'_'.date('Ymd_His', strtotime($request->get('datetime'))).'.aac';
+        $file_prefix = $request->get('feed').'_'.date('Ymd_His', strtotime($request->get('datetime')));
 
-        preg_match('/(.*?)_([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})\.aac/', $file, $matches);
+        preg_match('/(.*?)_([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{2})([0-9]{2})([0-9]{2})\.aac/', $file_prefix, $matches);
         $path = "$matches[2]/$matches[3]/$matches[4]/$matches[5]";
-        $filename = "$matches[1]_$matches[2]$matches[3]$matches[4]_$matches[5]0000.aac";
+        $filename = "$matches[1]_$matches[2]$matches[3]$matches[4]_$matches[5]0000";
+        $file = null; // the file we want to return
 
         // check if it exists in the temp bucket
-        if (!Storage::disk('recordings-temp')->exists($filename)) {
-            // if it does not, download from main bucket, decompress, upload to temp bucket
-            if (!Storage::disk('recordings')->exists("$path/$filename.xz")) {
-                return response('Error: No recording found at that time. Please try a different hour.', 404);
+        foreach (Storage::disk('recordings-temp')->files() as $tempFile) {
+            if (starts_with($tempFile, $filename)) {
+                $file = $tempFile;
+                break;
             }
-            Storage::put("recordings/$filename.xz", Storage::disk('recordings')->get("$path/$filename.xz"));
-            shell_exec('/usr/bin/xz -d ' .storage_path("app/recordings/$filename.xz"));
-            if (Storage::exists('recordings/'.$filename)) {
-                Storage::disk('recordings-temp')->put($filename, Storage::get('recordings/'.$filename));
-                Storage::delete(["recordings/$filename", "recordings/$filename.xz"]);
-            } else {
-                return response('Error: Could not decompress recording. Contact eric@crimeisdown.com for assistance.', 500);
+        }
+
+        if (!$file) {
+            foreach (Storage::disk('recordings')->files($path) as $audioFile) {
+                if (starts_with($audioFile, $filename)) {
+                    $file = $this->convertFile($path, $audioFile, $filename);
+                    break;
+                }
+            }
+            if (!$file) {
+                // We searched both buckets and can't find it
+                return response('Error: No recording found at that time. Please try a different hour.', 404);
             }
         }
 
@@ -99,5 +105,41 @@ class AudioArchiveController extends Controller
             ->object($file)
             ->signedUrl(now()->addDay());
         return response()->redirectTo($url);
+    }
+
+    private function convertFile($path, $filename, $file_prefix)
+    {
+        $extension = '';
+        if (ends_with($filename, '.aac.xz')) {
+            $extension = '.aac';
+            Storage::put("recordings/$filename", Storage::disk('recordings')->get("$path/$filename"));
+            shell_exec('/usr/bin/xz -d ' . storage_path("app/recordings/$filename"));
+            if (Storage::exists("recordings/$file_prefix$extension")) {
+                Storage::disk('recordings-temp')->put("$file_prefix$extension", Storage::get("recordings/$file_prefix$extension"));
+                Storage::delete(["recordings/$file_prefix$extension", "recordings/$filename"]);
+            } else {
+                return response('Error: Could not decompress recording. Contact eric@crimeisdown.com for assistance.',
+                    500);
+            }
+        } else if (ends_with($filename, '.ogg')) {
+            $opusSupported = false;
+            if ($opusSupported) {
+                $extension = '.ogg';
+                Storage::disk('recordings-temp')->put($filename, Storage::disk('recordings')->get("$path/$filename"));
+            } else {
+                $extension = '.mp3';
+                Storage::put("recordings/$filename", Storage::disk('recordings')->get("$path/$filename"));
+                $ffmpegPath = trim(shell_exec('which ffmpeg'));
+                shell_exec($ffmpegPath . ' -i ' . storage_path("app/recordings/$filename") . ' ' . storage_path("app/recordings/$file_prefix$extension"));
+                if (Storage::exists("recordings/$file_prefix$extension")) {
+                    Storage::disk('recordings-temp')->put("$file_prefix$extension", Storage::get("recordings/$file_prefix$extension"));
+                    Storage::delete(["recordings/$filename", "recordings/$file_prefix$extension"]);
+                } else {
+                    return response('Error: Could not convert recording to '.$extension.'. Contact eric@crimeisdown.com for assistance.',
+                        500);
+                }
+            }
+        }
+        return $file_prefix.$extension;
     }
 }
